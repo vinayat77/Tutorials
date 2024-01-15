200c48535674e44741a25573b5217c4e87495744
Vinay M <vinayyatwork@gmail.com>
2024-01-15 22:20:23 +0530

    Tutorials CRUD using Spring-boot, mysql and Angular.

---
+++ Tutorials/src/test/java/com/example/Tutorials/TutorialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tutorials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
